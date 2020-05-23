5e64cdf2ee80b037e04400d25efe7237f56d43a9
Martin Petron <martinpetron@outlook.com>
2020-05-23 12:50:18 -0300

    Updates

---
+++ precache-manifest.c80a03f067cbb3f5079620a3f16b923f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2f144e6d7bfac2ec2cc034d4ef3a3b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "01106df831f9d8070376",
+    "url": "/monster-rolodex/static/css/main.de626330.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01106df831f9d8070376",
+    "url": "/monster-rolodex/static/js/main.443f8cb9.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
